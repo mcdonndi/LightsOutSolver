c1230c8de7862fcd130f0e3b5f2d080a8bef1577
mcdonndi <mcdonndi@tcd.ie>
2017-11-21 22:46:53 +0000

    Refactored to use datatype Vector

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,15 @@
 --module Main where
 
-solvable :: [Int] -> Bool
+type Vector = [Int]
+type Row = [Int]
+type Matrix = [Row]
+
+solvable :: Vector -> Bool
 solvable initialConfig = if dot1 `mod` 2 == 0 && dot2 `mod` 2 == 0 then True else False
     where   dot1 = dotProduct initialConfig [0,1,1,1,0, 0,1,0,1,0, 1,1,0,1,1, 0,1,0,1,0, 0,1,1,1,0]
             dot2 = dotProduct initialConfig [1,0,1,0,1, 1,0,1,0,1, 0,0,0,0,0, 1,0,1,0,1, 1,0,1,0,1]
 
-dotProduct ::  [Int] -> [Int] -> Int
+dotProduct ::  Vector -> Vector -> Int
 dotProduct [] _ = 0
 dotProduct _ [] = 0
 dotProduct (x:xs) (y:ys) = x*y + dotProduct xs ys
