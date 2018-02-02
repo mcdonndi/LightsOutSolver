b73a3746e762e098e3c1f6f21b7d0b1cbaa88020
mcdonndi <mcdonndi@tcd.ie>
2018-02-02 18:22:33 +0000

    Added functions to rotate matrix and vector

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,13 @@
 --module Main where
 
+import Data.List
+
 type Vector = [Int]
 type Row = [Int]
 type Matrix = [Row]
+
+testMatrix :: Matrix
+testMatrix = [[0,1,0,1,0],[1,0,1,1,1],[0,1,1,1,0],[0,1,0,0,0],[0,0,0,1,1]]
 
 solvable :: Vector -> Bool
 solvable initialConfig = if dot1 `mod` 2 == 0 && dot2 `mod` 2 == 0 then True else False
@@ -19,3 +24,10 @@ reduceVectorTo23 v = take 23 v
 
 countMoves :: Vector -> Int
 countMoves v = sum v
+
+rotateMatrix :: Matrix -> Matrix
+rotateMatrix v = fn v
+    where fn = reverse . transpose . reverse . transpose
+
+flipVector :: Vector -> Vector
+flipVector v = reverse v
